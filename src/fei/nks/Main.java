95be8b95fcb664f528e306d3df174fbac8e163c1
z0045c9c <juraj.paska@siemens-healthineers.com>
2021-10-02 13:40:55 +0200

    refactor(hellman): refactor algorithms

--- src/fei/nks/Main.java
+++ src/fei/nks/Main.java
@@ -119,10 +119,11 @@ public class Main
         for(int i = 0; i < t; i++)
         {
             reducedHash = reduceHashToNDigitsAndPrependValue(hash, n, id);
+            reductions = i + 1;
             if(isReducedHashInTreeMap(reducedHash, map))
             {
-                System.out.println("Key: " + reducedHash + ", retrieved after :" + i + " reductions");
-                reductions = i + 1;
+                System.out.println("Key: " + reducedHash + ", retrieved after :" + reductions + " reductions is endpoint");
+                System.out.println("Searched Key is :" + reductions + " cols on the left from endpoint: " + reducedHash);
                 found = true;
                 break;
             }
@@ -141,26 +142,6 @@ public class Main
             }
             System.out.println(sp);
         }
-    }
-
-    private void test() throws NoSuchAlgorithmException
-    {
-        String id = "92318";
-        String pin = "000005";
-        String pattern = id + pin;
-        // reduce hash to pin with 6 numbers
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] encodedhash = digest.digest(pattern.getBytes(StandardCharsets.US_ASCII));
-        bruteforce(encodedhash);
-        System.out.println("end");
-
-        // comparators
-        //Comparator<String[]> comparator2 = (o1, o2) -> o1[o1.length - 1].compareTo(o2[o2.length - 1]);
-        //Comparator<String[]> comparator3 = Comparator.comparing(o -> o[o.length - 1]);
-
-//        Comparator<String> comparatorLambda = (o1, o2) -> o1.compareTo(o2);
-//        Comparator<String> comparatorNaturalOrder = Comparator.naturalOrder();
-//        Comparator<String> comparatorMethodReference = String::compareTo;
     }
 
     /**
@@ -212,6 +193,7 @@ public class Main
     {
         String id = "92318";
         int baseOfPin = 1_000_000;
+        int numberOfDigitsInPin = 6;
         // rows
         int m = 100;
         // cols
@@ -226,14 +208,14 @@ public class Main
 
         // test if image of key[2][96] is equal to key[2][97]
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        String keyToFind = arrayTable[2][96];
+        String keyToFind = arrayTable[2][99];
         byte[] hashFromKeyToFind = digest.digest(keyToFind.getBytes(StandardCharsets.US_ASCII));
         String check = arrayTable[2][97];
-        String reducedHash = reduceHashToNDigitsAndPrependValue(hashFromKeyToFind, 6, id);
+        String reducedHash = reduceHashToNDigitsAndPrependValue(hashFromKeyToFind, numberOfDigitsInPin, id);
 
         // try to find key from hash in table
         System.out.println("Searching for key: " + keyToFind);
-        tryAllEntries(hashFromKeyToFind, treeMap, t, 6, id);
+        tryAllEntries(hashFromKeyToFind, treeMap, t, numberOfDigitsInPin, id);
 
         System.out.println("end");
 
